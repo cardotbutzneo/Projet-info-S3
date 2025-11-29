c4988a38246439f3a729b7f16e5ca59d60874707
Adchayan <adchayan.jeyakumar@etu.cyu.fr>
2025-11-29 20:58:12 +0100

    Added Usine and AVL structures to avl.h
    Added Usine and AVL structures to avl.h

--- include/avl.h
+++ include/avl.h
@@ -6,4 +6,14 @@ typedef struct _noeud{
     int val;
 }Noeud;
 
+typedef struct Usine {
+    unsigned long identifiant;
+    unsigned long capacite;
+    unsigned long v_capte;
+    unsigned long v_traite;
+} Usine; 
+
+typedef struct AVL {
+    
+
 #endif
